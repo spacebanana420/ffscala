240451064850f87a21a2030d9c49ea822bdb3a76
Space Banana <tomasbessa@protonmail.com>
2023-08-30 21:20:31 +0100

    Fixes and additions

--- ffmpeg.scala
+++ ffmpeg.scala
@@ -11,40 +11,58 @@ private def belongsToList(text: String, group: List[String]): Boolean = {
 }
 
 private def indexFromList(text: String, group: List[String]): Int = {
-    for i <- group do {
-        if text == i then
+    for i <- 0 to group.length - 1 do {
+        if text == group(i) then
             return i
     }
     -1
 }
 
-private def isAlright_Image(cmd: List[String]): Boolean = {
-    var cmdstring: String = ""
+// private def isAlright_Image(cmd: List[String]): Boolean = {
+//     var cmdstring: String = ""
+//     val badArguments = List("-c:a", "-b:a", "-b:v", "-g", "-bf", "-crf")
+//     for i <- cmd do {
+//         cmdstring += i
+//     }
+//     for i <- badArguments do {
+//         if cmdstring.contains(i) == true then
+//             return false
+//     }
+//     true
+// }
+//
+// private def isAlright_Audio(cmd: List[String]): Boolean = {
+//     var cmdstring: String = ""
+//     val badArguments = List("-c:v", "-b:v", "-g", "-bf", "-crf", "-pix_fmt", "-filter:v", "-preset:v")
+//     for i <- cmd do {
+//         cmdstring += i
+//     }
+//     for i <- badArguments do {
+//         if cmdstring.contains(i) == true then
+//             return false
+//     }
+//     true
+// }
+
+private def isAlright_Image(args: String): Boolean = {
     val badArguments = List("-c:a", "-b:a", "-b:v", "-g", "-bf", "-crf")
-    for i <- cmd do {
-        cmdstring += i
-    }
     for i <- badArguments do {
-        if cmdstring.contains(i) == true then
+        if args.contains(i) == true then
             return false
     }
     true
 }
 
-private def isAlright_Audio(cmd: List[String]): Boolean = {
-    var cmdstring: String = ""
-    val badArguments = List("-c:v", "-b:v", "-g", "-bf", "-crf", "-pix_fmt")
-    for i <- cmd do {
-        cmdstring += i
-    }
+private def isAlright_Audio(args: String): Boolean = {
+    val badArguments = List("-c:v", "-b:v", "-g", "-bf", "-crf", "-pix_fmt", "-filter:v", "-preset:v")
     for i <- badArguments do {
-        if cmdstring.contains(i) == true then
+        if args.contains(i) == true then
             return false
     }
     true
 }
 
-private def stringToList(text: String): Array[String] = {
+private def stringToList(text: String): List[String] = {
     var stringArray: Array[String] = Array()
     var arg = ""
     for i <- text do {
@@ -109,6 +127,44 @@ def setVideoBitrate(method: Byte, bitrate: Int, dimension: String): String = {
             ""
 }
 
+def setVideoResolution(width: Int, height: Int): String = {
+    if width <= 0 || height <= 0 then
+        ""
+    else
+        "-filter:v scale=" + width + ":" + height
+}
+
+def setScaleFilter(filter: String): String = {
+    val supportedFilters = List("bicubic", "bilinear", "neighbour")
+    val foundFilter = belongsToList(filter, supportedFilters)
+    if foundFilter == false then
+        ""
+    else
+        "-sws_flags " + filter
+}
+
+def setKeyframeInterval(interval: Int): String = {
+    if interval < 0 then
+        return ""
+    "-g " + interval
+}
+
+def setBFrames(interval: Byte): String = {
+    if interval < 0 || interval > 16 then
+        return ""
+    "-bf " + interval
+}
+
+def setPixFmt(newfmt: String): String = { //needs to check for variants like rgb48be
+    val supportedFormats = List("rgb24", "rgb8", "rgb48", "gray", "yuv420p", "yuv422p", "yuv444p")
+    //val ffmpegEquivalents = List("rgb24", "rgb8", "rgb48", "gray", "yuv420p", "yuv422p", "yuv444p")
+    val foundformat = belongsToList(newfmt, supportedFormats)
+
+    if foundformat == false then
+        return ""
+    "-pix_fmt " + newfmt
+}
+
 def setAudioEncoder(encoder: String): String = {
     val supportedFormats = List("copy", "aac", "opus", "vorbis", "mp3", "ac3", "flac", "pcm16", "pcm24", "pcm32")
     val ffmpegEquivalents = List("copy", "aac", "libopus", "libvorbis", "libmp3lame", "ac3", "flac", "pcm_s16le", "pcm_s24le", "pcm_s32le")
@@ -144,39 +200,11 @@ def setOutput(name: String, format: String): String = {
     if name == "" || format == "" then
         return ""
 
-    val supportedFormats = List("png", "jpeg", "jpg", "tiff", "tif", "bmp", "gif", "webp", "mp4", "mov",
-    "m4v", "avi", "mkv", "webm", "flac", "wav", "ogg", "opus", "m4a", "mp3")
+    val supportedFormats = List("png", "apng", "avif", "jpeg", "jpg", "tiff", "tif", "bmp", "gif", "webp", "mp4", "mov",
+    "m4v", "avi", "mkv", "webm", "flac", "wav", "ogg", "opus", "m4a", "mp3", "aiff")
     val isSupported = belongsToList(format, supportedFormats)
 
     if isSupported == false then
         return ""
     name + "." + format
-}
-
-def setVideoResolution(width: Int, height: Int): String = {
-    if width == 0 then
-        return ""
-    "-filter:v scale=" + width + ":" + height
-}
-
-def setKeyframeInterval(interval: Int): String = {
-    if interval < 0 then
-        return ""
-    "-g " + interval
-}
-
-def setBFrames(interval: Byte): String = {
-    if interval < 0 || interval > 16 then
-        return ""
-    "-bf " + interval
-}
-
-def setPixFmt(newfmt: String): String = { //needs to check for variants like rgb48be
-    val supportedFormats = List("rgb24", "rgb8", "rgb48", "gray", "yuv420p", "yuv422p", "yuv444p")
-    //val ffmpegEquivalents = List("rgb24", "rgb8", "rgb48", "gray", "yuv420p", "yuv422p", "yuv444p")
-    val foundformat = belongsToList(newfmt, supportedFormats)
-
-    if foundformat == false then
-        return ""
-    "-pix_fmt " + newfmt
 }
